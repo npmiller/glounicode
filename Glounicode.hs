e7bc3c22f93d9775096af1f2d503c52ef241da45
npmiller <nicolas.pierre.miller@gmail.com>
2014-11-22 23:02:17 +0100

    Various lisibility improvements

--- Glounicode.hs
+++ Glounicode.hs
@@ -2,44 +2,57 @@ module Glounicode (glouencode, gloudecode) where
 
 import Data.Char (chr, ord)
 
+{- Encoding -}
 glouencode :: String -> [String]
 glouencode x = concatMap glouencodeOne (map ord x)
 
-gloudecode :: [String] -> String
-gloudecode l = gloudecode' l []
-
-gloudecode' [] d = d
-gloudecode' (x:xs) d = gloudecode' (drop (nb+1) (x:xs)) (d ++ [chr (gloudecodeOne nb (take (nb+1) (x:xs)))])
-	where
-		nb = nbGlou x
-
+glouencodeOne :: Int -> [String]
 glouencodeOne n = encodeGlouNum (length l) $ glouEncodeWords l n []
 	where
 		l = glouFactors n
 
-encodeGlouNum nb (x:xs) = ("g" ++ take nb (cycle "l") ++ drop 1 x):xs
-
-glouEncodeWords [] nb l = l ++ [intToGlou nb]
-glouEncodeWords (x:xs) 0 l = glouEncodeWords xs 0 (l ++ [intToGlou 0])
+glouEncodeWords :: [Int] -> Int -> [String] -> [String]
+glouEncodeWords []     nb l = l ++ [intToGlou nb]
+glouEncodeWords (x:xs) 0  l = glouEncodeWords xs 0            (l ++ [intToGlou 0])
 glouEncodeWords (x:xs) nb l = glouEncodeWords xs (nb `mod` x) (l ++ [intToGlou (nb `div` x)])
 
+encodeGlouNum :: Int -> [String] -> [String]
+encodeGlouNum nb (x:xs) = ('g' : take nb (cycle "l") ++ drop 1 x):xs
+
+glouFactors :: Int -> [Int]
 glouFactors nb
-	| nb < 128 = reverse $ takeWhile (< nb) (128:[64^2,64^3..])
+	| nb < 128 = []
 	| otherwise = reverse $ takeWhile (< nb) (64:[64^2,64^3..])
 
-gloudecodeOne nb l = foldl (\acc (n,i) ->  acc + adaptToPos (nb - i) n) 0 glouint 
+
+{- Decoding -}
+gloudecode :: [String] -> String
+gloudecode l = gloudecode' l ""
+
+gloudecode' :: [String] -> String -> String
+gloudecode' [] r = r
+gloudecode' (x:xs) r = gloudecode' (drop (nb+1) (x:xs)) (r ++ [chr (gloudecodeOne nb (take (nb+1) (x:xs)))])
+	where
+		nb = nbGlou x
+
+gloudecodeOne :: Int -> [String] -> Int
+gloudecodeOne nb l = foldl (\acc (n,i) ->  acc + adaptToPos (nb - i) n) 0 glouint
 	where
 		glouint = zip (map glouToInt l) [0,1..]
 
-nbGlou ('g':'l':x) = countElem x 'l'
+{- Utilities functions -}
+nbGlou :: String -> Int
+nbGlou ('g':'l':x) = countElem 'l' x
 
-intToGlou x = 'g': 'l': "o" ++ take o (cycle "o") ++ "u" ++ take u (cycle "u")
-	where 
-		o = x `div` 10
-		u = x `mod` 10
+intToGlou :: Int -> String
+intToGlou x = "glo" ++ take o (cycle "o") ++ "u" ++ take u (cycle "u")
+	where (o,u) = x `divMod` 10
 
-glouToInt ('g':'l':s) =  10 * (countElem s 'o' - 1) + (countElem s 'u' - 1)
+glouToInt :: String -> Int
+glouToInt ('g':'l':s) =  10 * (countElem 'o' s - 1) + countElem 'u' s - 1
 
-countElem l e = length $ filter (== e) l
+countElem :: (Eq a) => a -> [a] -> Int
+countElem e = length . filter (== e)
 
+adaptToPos :: Int -> Int -> Int
 adaptToPos nb n = n * (2 ^ (6 * nb))
