2a6841981aaa3da9fca7b31d9a02fbedea52f639
npmiller <nicolas.pierre.miller@gmail.com>
2015-01-30 22:05:11 +0100

    Add content type while serving html files

--- web/Main.hs
+++ web/Main.hs
@@ -6,17 +6,20 @@ import qualified Data.Text.Lazy as T
 import Glounicode
 
 main = scotty 3000 $ do
-  get "/" $
+  get "/" $ do
+    setHeader "content-type" "text/html"
     file "glounicode.html"
 
-  get "/encode" $
+  get "/encode" $ do
+    setHeader "content-type" "text/html"
     file "glouencode.html"
 
   post "/encode" $ do
     paramString <- param "data"
     text . T.pack $ unwords $ glouencode paramString
 
-  get "/decode" $
+  get "/decode" $ do
+    setHeader "content-type" "text/html"
     file "gloudecode.html"
 
   post "/decode" $ do
